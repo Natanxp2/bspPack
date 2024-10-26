2fca22e747c3c7c0e896cceeba7b92d2c04af1ab
Natanxp2 <natanb97@gmail.com>
2024-10-26 22:43:47 +0200

    Added support for modify flag which replaces include, includeDir, exclude, excludeDir, and excludevpk flags

--- Config.cs
+++ Config.cs
@@ -3,15 +3,26 @@ using System.IO;
 
 namespace BSPPackStandalone
 {
-    public static class Config
-    {
+	public static class Config
+	{
 		public static string BSPFile { get; set; }
-        public static readonly string GameFolder = @"E:\__STEAM__\steamapps\common\Momentum Mod Playtest\momentum";
-        public static readonly string SteamAppsPath = GameFolder.Substring(0, GameFolder.IndexOf("steamapps") + 9);
-        public static readonly string BSPZip = Path.Combine(GameFolder, @"..\bin\win64", "bspzip.exe");
-        public static readonly string KeysFolder = Path.Combine(Directory.GetCurrentDirectory(), "Keys");
-        public static readonly string TempFolder = Path.Combine(Directory.GetCurrentDirectory(), "Temp");
-        public static readonly string CopyLocation = Path.Combine(Directory.GetCurrentDirectory()); //Wtf is this?
-        public static readonly string VPK = Path.Combine(Directory.GetCurrentDirectory()); //Wtf is this?
-    }
+		public static string GameFolder { get; set; } 
+		public static string SteamAppsPath { get; set; }
+		
+		public static string BSPZip { get; private set; }
+		public static string KeysFolder { get; private set; }
+		public static string TempFolder { get; private set; }
+		public static string CopyLocation { get; private set; }
+		public static string VPK { get; private set; }
+
+		public static void InitializeConfig()
+		{
+			// SteamAppsPath = GameFolder.Substring(0, GameFolder.IndexOf("steamapps") + 9);
+			BSPZip = Path.Combine(GameFolder, @"..\bin\win64", "bspzip.exe");
+			KeysFolder = Path.Combine(Directory.GetCurrentDirectory(), "Keys");
+			TempFolder = Path.Combine(Directory.GetCurrentDirectory(), "Temp");
+			CopyLocation = Path.Combine(Directory.GetCurrentDirectory()); //Placeholder
+			VPK = Path.Combine(Directory.GetCurrentDirectory()); //Placeholder
+		}
+	}
 }
